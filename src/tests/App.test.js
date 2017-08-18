4a30660335b0f016e9f96c98c3eaac1585cb6b9d
Loganathan Sellappa <Loganathan.Sellappa@logmein.com>
2017-08-18 18:45:17 +0530

    Test cases added for App component

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import App from '../components/App';
+import Header from '../components/Header';
+import TaskForm from '../components/TaskForm';
+import Task from '../components/Task';
+import Footer from '../components/Footer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe(App, () => {
+	// Added to allow lifecycles, bydefault Enzyme wont execute lifecylce methods
+	const options = {
+  		lifecycleExperimental: true,
+  		disableLifecycleMethods: false 
+	};
+
+  const component = shallow(<App />, options);
+
+  it('renders and matches our snapshot', () => {
+    const component = renderer.create(<App />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('contains an Header subcomponent', () => {
+    expect(component.find(Header)).toHaveLength(1);
+  });
+
+  it('contains an TaskForm subcomponent', () => {
+    expect(component.find(TaskForm)).toHaveLength(1);
+  });
+
+  it('contains an Task subcomponent', () => {
+    expect(component.find(Task)).toHaveLength(9);
+  });
+
+  it('contains an Footer subcomponent', () => {
+    expect(component.find(Footer)).toHaveLength(1);
+  });
+
+  it('contains the same number of Task components as tasks', () => {
+  	const tasks = component.find(Task).length;
+  	const s_tasks   = Object.keys(component.state('tasks')).length;
+  	expect(tasks).toEqual(s_tasks);
+  });
+
 });
